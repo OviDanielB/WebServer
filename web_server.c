afddd079acace0984f830762055f97cbcb562f19
OviDanielB <ovi.daniel.b@gmail.com>
2016-03-18 19:47:22 +0100

    first preforking

--- web_server.c
+++ web_server.c
@@ -1,9 +1,3 @@
-/*#include <stdio.h>
-
-int main(void){
-    printf("Ciao");
-    return 0;
-}*/
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -17,6 +11,7 @@ int main(void){
 
 #define DEFAULT_PORT       5193            /* default protocol port number */
 #define BACKLOG           10            /* size of request queue        */
+#define MAXLINE             1024
 
 
 /*------------------------------------------------------------------------
@@ -32,75 +27,87 @@ int main(void){
  *            the server uses the default given by PROTOPORT.
  *------------------------------------------------------------------------ */
 
+static int num_child;
+static pid_t *pids;
+
 int main(int argc, char **argv)
 {
-    struct  protoent *ptrp;  /* pointer to a protocol table entry   */
-    struct  sockaddr_in sad; /* structure to hold server's address  */
-    struct  sockaddr_in cad; /* structure to hold client's address  */
-    int     listensd, connsd; /* socket descriptors             */
-    int     port;        /* protocol port number        */
-    unsigned int    alen;        /* length of address           */
-    char 	  sendBuf[1024];    /* buffer for data the server sends */
+    int listensd, connsd, i;
+    struct sockaddr_in servaddr;
+    char buff[MAXLINE];
+    socklen_t addrlen = 2;
 
-    memset((void *)&sad, 0, sizeof(sad)); /* clear sad */
-    sad.sin_family = AF_INET;     /* set family to Internet */
-    sad.sin_addr.s_addr = htonl(INADDR_ANY); /* set the local IP address */
+    in_port_t port;
+    time_t ticks;
 
-    /* Check command-line argument for protocol port and extract
-       port number if one is specified.  Otherwise, use the default
-       port value given by constant DEFAULT_PORT. */
+    pid_t child_make(int, int, int);
 
-    if (argc > 1)           /* if argument specified    */
-        port = atoi(argv[1]);   /* convert argument to binary   */
-    else
-        port = DEFAULT_PORT;     /* use default port number      */
-
-    if (port > 0)           /* test for illegal value   */
-        sad.sin_port = htons(port);
-    else {              /* print error message and exit */
-        fprintf(stderr,"bad port number %s\n",argv[1]);
-        exit(1);
+    // TODO arguments
+    if(argc <= 2){
+        // if not specified, use default port
+        port = (in_port_t) DEFAULT_PORT;
+    } else if(argc == 3) {
+        // use specified port
+        port = (in_port_t ) argv[2];
+    } else {
+        printf("Usage : ./web_server <ip Address> <port number>");
+        exit(EXIT_FAILURE);
     }
 
-    /* Map TCP transport protocol name to protocol number */
-    if ( (ptrp = getprotobyname("TCP")) == NULL) {
-        fprintf(stderr, "cannot map \"TCP\" to protocol number");
-        exit(1);
+    // creates a listening socket
+    if((listensd=socket(AF_INET,SOCK_STREAM,0)) < 0){
+        perror("Error in listening socket");
+        exit(EXIT_FAILURE);
     }
 
-    /* Create a socket */
-    if ((listensd = socket(AF_INET, SOCK_STREAM, ptrp->p_proto)) < 0) {
-        perror("socket creation failed");
-        exit(1);
+    // set all bytes of the sockaddr_in struct to 0 with memset
+    memset((void *)&servaddr,0,sizeof(servaddr));
+    // initialize all struct fields
+    servaddr.sin_family = AF_INET; // AF_INET = IPv4 with 32-bit address and 16-bit port number (in_port_t)
+    // INADDR_ANY allows the program to work without knowing the ip address of the machine it is running on
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // returns host_uint32 converted to network byte order
+    servaddr.sin_port = htons(port); // returns host_uint16 converted to network byte order
+
+    // bind the listening socket to the address; needs casting to sockaddr *
+    if(bind(listensd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+        perror("Error in Bind");
+        exit(EXIT_FAILURE);
     }
 
-    /* Bind a local address to the socket */
-    if (bind(listensd, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
-        perror("bind failed");
-        exit(1);
+    // marks the socket as a passive socket and it is ready to accept connections
+    // BACKLOG max number of allowerd connections. if max reached the user will get an error
+    if(listen(listensd,BACKLOG) < 0){
+        perror("Error in listen");
+        exit(EXIT_FAILURE);
     }
 
-    /* Specify size of request queue */
-    if (listen(listensd, BACKLOG) < 0) {
-        perror("listen failed");
-        exit(1);
+    // creates memory for pids
+    pids = calloc( (size_t) num_child,sizeof(pid_t));
+
+    // create lock file for child processes
+    // my_lock_init("filename");
+
+    num_child = 4;
+    // create pids array with children pids
+    for(i = 0; i < num_child; i++ ){
+        pids[i] = child_make(i, listensd, addrlen);
     }
 
-    /* Main server loop - accept and handle requests */
-    while (1) {
-        alen = sizeof(cad);
-        if ( (connsd=accept(listensd, (struct sockaddr *)&cad, &alen)) < 0) {
-            perror("accept failed");
-            exit(1);
-        }
+    printf("Finished %d\n", getpid());
 
-        //do server work
+    return 0;
 
-        if (write(connsd, sendBuf, strlen(sendBuf)) != strlen(sendBuf)) {
-            perror("error in write");
-            exit(1);
-        }
 
-        close(connsd);
+}
+
+pid_t child_make(int i, int listenfd, int addrlen){
+    pid_t pid;
+
+    // if it's father return pid
+    if((pid = fork()) > 0){
+        return pid;
     }
+
+    printf("CIao %d\n",getpid());
+
 }
